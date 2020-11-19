bbd18f0a490b813eacbb590f255cb55af2b776f2
Andy Linton <asjl@lpnz.org>
2020-11-19 14:03:54 +1300

    modified:   js/webpage_tools.js

--- js/webpage_tools.js
+++ js/webpage_tools.js
@@ -65,17 +65,17 @@ function enable_button() {
 function show_iframe(url) {
     console.log(url);
     // Add other sources as needed
-    if (url.startsWith('https://www.youtube.com')) {
+    if (url.startsWith('https://www.youtube.com/')) {
         url = url.replace('&t=', '?start=');
         document.write('<div class="container-iframe"><iframe   class="responsive-iframe" src="https://www.youtube.com/embed/' + url.split('v=')[1] + '" frameborder="0" allowfullscreen></iframe></div>');
     }
-    else if (url.startsWith('https://www.facebook.com')) {
+    else if (url.startsWith('https://www.facebook.com/')) {
         document.write('<div class="container-iframe"><iframe   class="responsive-iframe" src="https://www.facebook.com/plugins/video.php?href=' + encodeURI(url) + '&show_text=0&mute=0"  style="border:none;overflow:hidden" scrolling="no" frameborder="0" allowTransparency="true" allowFullScreen="false"></iframe></div>');
     }
-    else if (url.startsWith('https://vimeo.com')) {
+    else if (url.startsWith('https://vimeo.com/')) {
         document.write('<div class="container-iframe"><iframe   class="responsive-iframe" src="https://player.vimeo.com/video/' + url.split('vimeo.com/')[1] + '" frameborder="0" webkitallowfullscreen mozallowfullscreen allowfullscreen></iframe></div>');
     }
-    else if (url.startsWith('https://soundcloud.com')) {
+    else if (url.startsWith('https://soundcloud.com/')) {
         document.write('<div class="container-iframe"><iframe   class="responsive-iframe" src="https://w.soundcloud.com/player/?url=' + encodeURI(url) + '&hide_related=true" width="100%"></iframe></div>');
     }
     else if (url.startsWith('https://media.comhaltas.ie/video/')) {
